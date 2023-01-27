aa8009ecf4466bee1723dd4a68723341ca9f4542
Andrew <andrew.charabin@voices.com>
2023-01-26 19:07:58 -0500

    fixed about me formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,7 @@ function showSkills(skills) {
         skillHTML += `
         <div class="bar">
               <div class="info">
-                <img src=${skill.icon} alt="skill" />
+                <img src=${skill.iconprefix}${skill.icon} alt="skill" />
                 <span>${skill.name}</span>
               </div>
             </div>`
